71653dc0491b285911f4269960c49d107ac5bd51
kenkoii <jankenith.johannessen@gmail.com>
2016-12-01 13:49:31 +0800

    First commit, very buggy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
+import { AgmCoreModule } from 'angular2-google-maps/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { MapComponent } from './map/map.component';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyDdOFLMpJvyMz4eyUCnUjBBYH-kSxBOwT0",
+    authDomain: "testing-a7506.firebaseapp.com",
+    databaseURL: "https://testing-a7506.firebaseio.com",
+    storageBucket: "testing-a7506.appspot.com"
+};
+
+const myFirebaseAuthConfig = {
+  provider: AuthProviders.Google,
+  method: AuthMethods.Popup
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig, myFirebaseAuthConfig),
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBAe8thTIIikNvgLp7DDLNK0OeFOUyLf9o'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
